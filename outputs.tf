94700fd46278710e09dd7db6302374a11b90ec3f
Sri Lakshmi Manikandan <sriksh26@gmail.com>
2026-09-25 09:19:19 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,10 @@
-
+output "instance_public_ip" {
+    description = "public ip of the ec2 instance"
+    value=aws_instance.nginx-server.public_ip
+  
+}
+output "instance-url" {
+    description = "url to access the nginx server"
+    value="http://${aws_instance.nginx-server.public_ip}"
+  
+}
